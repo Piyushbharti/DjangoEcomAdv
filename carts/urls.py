c1ae7e478a5c5a48ff4f77634295fdfd2ab0a898
Piyush Bharti <piyush.bharti@snva.com>
2026-01-05 19:31:01 +0530

    delete cartItem

--- carts/urls.py
+++ carts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import temp, add_cart, cart, remove_cart
+from .views import temp, add_cart, cart, remove_cart, delete_cart
 urlpatterns = [
      path('', temp),
      path('addProduct/<int:product_id>', add_cart),
      path('allCartItem', cart),
      path('removeCartItem/<int:product_id>', remove_cart),
+     path('deleteCartItem/<int:product_id>', delete_cart),
 ]
