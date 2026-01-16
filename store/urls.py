3a99bc41b608b93a7a4169633c4a81d8bc3eb6df
Piyush <piyush.bharti@snva.com>
2026-01-16 19:15:44 +0530

    variation serializer added

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import getAllProduct, postNewProduct, update_product, getProductByCat,getAllProductByPagination, searchProduct
+from .views import getAllProduct, postNewProduct, update_product, getProductByCat,getAllProductByPagination, searchProduct, getSingleProductByCatV2
 
 urlpatterns = [
     path('getAllProduct/', getAllProduct),
     path('addProduct/', postNewProduct),
     path('updateProduct/<int:product_id>', update_product),
     path('productBySlug/<slug:slug>', getProductByCat),
+    path('productBySlugv2/<slug:slug>', getSingleProductByCatV2),
     path('allProduct/', getAllProductByPagination),
     path('search/', searchProduct),
 ]
