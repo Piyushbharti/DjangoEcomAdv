920e11e3daf201f0bb3c861cfcf050d7e2882d62
Piyush <piyush.bharti@snva.com>
2026-05-15 18:55:44 +0530

    order model created

--- orders/models.py
+++ orders/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from accounts.models import Account
+from django.utils import timezone
 
 # Create your models here.
 class Order(models.Model):
@@ -13,11 +15,16 @@ class Order(models.Model):
     def save(self, *args, **kwargs):
         if not self.order_number:
             self.order_number = self._generate_order_number()
-    super().save(*args, **kwargs)
+        super().save(*args, **kwargs)
 
     def  _generate_order_number(self):
         year = timezone.now().year
-        pass
-
+        lastOrd = Order.objects.filter(order_number__startswith = f"ORD-{year}").order_by('-order_number').first()
+        if lastOrd:
+            last_num = int(lastOrd.order_number.split('-')[-1])
+            new_num = last_num + 1
+        else:
+            new_num = 1
+        return f"ORD-{year}-{new_num:05d}"
     def __str__(self):
-        return self.name
+        return self.order_number
